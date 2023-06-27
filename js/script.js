be63277f429567d739baf4aa8feccf52ac9d9b7e
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-27 16:06:07 +0200

    test con una mail

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            
+            randomMail: null
         }
+    },
+
+    created(){
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then(res => {
+            this.randomMail = res.data.response;
+        })
     }
+
 }).mount('#root');
